5d52cb2cd903c1c9c41b5e237a5b7cc0ea99e862
Марина Разумова <marina.razumova.19@yandex.ru>
2022-01-23 14:55:10 +0300

    Добавьте файлы проекта.

---
+++ WpfApp19.1/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp19._1.Models
+{
+    static class Ariph
+    {
+       public  static double Add(double a) => a* Math.PI * 2;
+    }
+}
